8045b5b9b2dec18d85ef8e98aa1bc5cafbd1c992
linh lou <dang.tn.linh@gmail.com>
2023-09-04 11:08:04 +0200

    test submit Form

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -11,6 +11,7 @@ import { localStorageMock } from "../__mocks__/localStorage.js"
 import { bills } from "../fixtures/bills.js"
 
 
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     test("Then the formular is shown", () => {
@@ -40,18 +41,17 @@ describe("Given I am connected as an employee", () => {
   })
   describe('When I am on NewBill page and I choose a file',()=>{
     test('Then only image will be selected, other type of file will not be accepted', ()=>{
-      document.body.innerHTML=''
       Object.defineProperty(window,'localStorage', {value: localStorageMock})
       window.localStorage.setItem('user',JSON.stringify({
         type: "Employee",
         email: "employee@test.tld"
       }))
-      const onNavigate = (path_name)=>{
-        document.body.innerHTML = ROUTES({path_name})
+      const onNavigate = (pathname)=>{
+        document.body.innerHTML = ROUTES({ pathname })
       }
       document.body.innerHTML = NewBillUI()
       const newBill = new NewBill({document, onNavigate, store: mockStore, localStorage: window.localStorage})
-      const handleChangeFile = jest.fn(()=>newBill.handleChangeFile)
+      const handleChangeFile = jest.fn(()=> newBill.handleChangeFile)
       const fileNode = screen.getByTestId('file')
       fileNode.addEventListener('change',handleChangeFile)
       fireEvent.change(fileNode,{
@@ -76,21 +76,22 @@ describe("Given I am connected as an employee", () => {
 
 
 describe("Given I am connected as an employee, and I am on NewBill page",()=>{
-  describe('When I click to submit button',()=>{
+  describe('When I submit form',()=>{
     test('Then the Bills page is renderd', async ()=>{
-      Object.defineProperty(window, 'localStorage', {value:localStorageMock})
+      Object.defineProperty(window, 'localStorage', { value:localStorageMock })
       window.localStorage.setItem('user',JSON.stringify({
         type: "Employee",
         email: "employee@test.tld"
       }))
   
-      const onNavigate = (path_name)=>{
-        document.body.innerHTML = ROUTES({ path_name, bills})
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname, data:bills})
       }
-  
+      
       document.body.innerHTML = NewBillUI()
-
+  
       const newBill = new NewBill({document, onNavigate, store:mockStore, localStorage: window.localStorage})
+
       const fileNode = screen.getByTestId('file')
       fireEvent.change(fileNode,{
         target: {
@@ -104,19 +105,21 @@ describe("Given I am connected as an employee, and I am on NewBill page",()=>{
       const pct = screen.getByTestId('pct')
       const date = screen.getByTestId('datepicker')
       const expense_type = screen.getByTestId('expense-type')
-      fireEvent.change(expense_name,{target:{value:'helo'}})
-      fireEvent.change(commentary,{target:{value:'helllo'}})
+      fireEvent.change(expense_name,{target:{value:'test'}})
+      fireEvent.change(commentary,{target:{value:'just for test'}})
       fireEvent.change(amount,{target:{value:200}})
       fireEvent.change(vat,{target:{value:20}})
       fireEvent.change(pct,{target:{value:10}})
       fireEvent.change(date,{target:{value:'2020-05-12'}})
       fireEvent.change(expense_type,{target:{value:'Transports'}})
 
-      const btnSubmit =  screen.getByTestId('btn-send-bill')
+      const form =  screen.getByTestId('form-new-bill')
       const handleSubmit = jest.fn(()=>newBill.handleSubmit)
-      btnSubmit.addEventListener('click',handleSubmit)
-      fireEvent.click(btnSubmit)
+      form.addEventListener('submit',handleSubmit)
+      fireEvent.submit(form)
       expect(handleSubmit).toHaveBeenCalled()
+      const billsPage = screen.getAllByText('Mes notes de frais')
+      expect(billsPage).toBeTruthy()
 
     })
   })
